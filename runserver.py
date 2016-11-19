ad3ddb033bafc478c9a27c7cded2c7cee59a66ce
Benjamin Schubert <ben.c.schubert@gmail.com>
2016-11-19 22:29:30 +0100

    added livereload and index.html

--- runserver.py
+++ runserver.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import logging
+import os
+
 import tornado.ioloop
 import tornado.options
 import tornado.web
@@ -21,9 +23,19 @@ logger = logging.getLogger("tornado.application")
 
 data = Data()
 
+static = os.path.join(os.path.dirname(__file__), "static")
+
 
 def make_app():
+    settings = {
+        "static_path": static,
+        "cookie_secret": "THIS_SHOULD_NEVER_GET_PUSHED_TO_GITHUB",
+        "xsrf_cookies": True,
+        "autoreload": True,
+    }
+
     return tornado.web.Application([
+        (r"^()/$", tornado.web.StaticFileHandler, dict(default_filename="index.html", path=static)),
         (r"^/actors$", ActorsHandler, dict(data=data)),
         (r"^/actors/([0-9]+)$", ActorDetailsHandler, dict(data=data)),
         (r"^/actors/([0-9]+)/actions$", ActorActionsHandler, dict(data=data)),
@@ -32,7 +44,7 @@ def make_app():
         (r"^/sensors/([a-zA-Z0-9]+)/event$", EventHandler, dict(data=data)),
         (r"^/link/$", LinkHandler, dict(data=data)),
         (r"^/link/([0-9]+)$", LinkDetailsHandler, dict(data=data)),
-    ])
+    ], **settings)
 
 
 if __name__ == '__main__':
